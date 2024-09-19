3a91dee4d4f92e685f5c0e32267a7aca5794f924
Malagutt1 <kauamarcosmalagutti2003@gmail.com>
2024-09-19 15:21:34 -0300

    commit variaveis_tipos_de _comentarios
    19/09

--- exercicios/variaveis_tipos_comentario.py
+++ exercicios/variaveis_tipos_comentario.py
@@ -1,17 +1,32 @@
-#Crie um comentário de no máximo uma linha.
-2. Crie um comentário de mais de uma linha.
-3. Escreva um programa que mostra em tela a mensagem: Boa tarde pessoal da disciplina
-de Programação II.
-4. Crie 03 variáveis com três tipos de dados diferentes, observando a sintaxe da
-linguagem Python.
-5. Defina uma variável exiba em tela o valor e o tipo de dado dessa variável.
-6. Solicite para que o usuário digite um número, em seguida exiba em tela o número
-digitado.
-7. Crie duas variáveis. Atribua valores numéricos a elas. Some os valores dessas variáveis.
-Por fim exiba em tela o resultado da soma.
-8. Crie duas variáveis. Atribua valores numéricos a elas. Subtraia os valores dessas
-variáveis. Por fim exiba em tela o resultado da subtração.
-9. Crie duas variáveis. Atribua valores numéricos a elas. Multiplique os valores dessas
-variáveis. Por fim exiba em tela o resultado da multiplicação.
-10. Crie duas variáveis. Atribua valores numéricos a elas. Divida os valores dessas
-variáveis. Por fim exiba em tela o resultado da divisão.
+#1.Crie um comentário de no máximo uma linha.
+    #Primeiro comentario na linguagem Python
+
+#2. Crie um comentário de mais de uma linha.
+"""Pra que serve 
+   tantos codigos?
+   se a vida
+   não é programada
+   e as melhores coisas
+   não tem logica"""
+
+
+#3. Escreva um programa que mostra em tela a mensagem: Boa tarde pessoal da disciplina de Programação II.
+ print("Boa tarde pessoal da disciplina de Programação II")
+
+#4. Crie 03 variáveis com três tipos de dados diferentes, observando a sintaxe da linguagem Python.
+    num1=1
+    num2=2
+    num3=3
+
+#5. Defina uma variável que exiba em tela o valor e o tipo de dado dessa variável.
+Print
+
+#6. Solicite para que o usuário digite um número, em seguida exiba em tela o número digitado.
+
+#7. Crie duas variáveis. Atribua valores numéricos a elas. Some os valores dessas variáveis. Por fim exiba em tela o resultado da soma.
+
+#8. Crie duas variáveis. Atribua valores numéricos a elas. Subtraia os valores dessas variáveis. Por fim exiba em tela o resultado da subtração.
+
+#9. Crie duas variáveis. Atribua valores numéricos a elas. Multiplique os valores dessas variáveis. Por fim exiba em tela o resultado da multiplicação.
+
+#10. Crie duas variáveis. Atribua valores numéricos a elas. Divida os valores dessas variáveis. Por fim exiba em tela o resultado da divisão.
