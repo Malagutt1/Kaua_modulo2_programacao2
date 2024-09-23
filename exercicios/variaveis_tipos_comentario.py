edbecb63396cd9f62aa48bde2587ea0fc7558cd0
_Malagutt1_ <kauamarcosmalagutti2003@gmail.com>
2024-09-23 15:59:46 -0300

    commit exercicios
    resolução das 10 questões

--- exercicios/variaveis_tipos_comentario.py
+++ exercicios/variaveis_tipos_comentario.py
@@ -15,18 +15,40 @@
 
 #4. Crie 03 variáveis com três tipos de dados diferentes, observando a sintaxe da linguagem Python.
     num1=1
-    num2=2
-    num3=3
-
+    palavra=input("olá mundo")
+    numero_digitado_por_usuario=int(input("Digire um numero:  "))
+    
 #5. Defina uma variável que exiba em tela o valor e o tipo de dado dessa variável.
-Print
+num0=5
+print("O valor é: ",num0)
+
+               ###Como faz para aparecer o dado da variavel?
 
 #6. Solicite para que o usuário digite um número, em seguida exiba em tela o número digitado.
+numero_digitado=int(input("Digite um numero: \n"))
+print("Você digitou:", numero_digitado)
+
 
 #7. Crie duas variáveis. Atribua valores numéricos a elas. Some os valores dessas variáveis. Por fim exiba em tela o resultado da soma.
+variavel1= 5
+variavel2=7
+soma= variavel1+variavel2
+print("A soma é de:", soma)
 
 #8. Crie duas variáveis. Atribua valores numéricos a elas. Subtraia os valores dessas variáveis. Por fim exiba em tela o resultado da subtração.
+variavel1= 5
+variavel2=7
+subtracao= variavel1-variavel2
+print("A subtração é de:", subtracao)
 
 #9. Crie duas variáveis. Atribua valores numéricos a elas. Multiplique os valores dessas variáveis. Por fim exiba em tela o resultado da multiplicação.
+variavel1= 5
+variavel2=7
+multi= variavel1*variavel2
+print("A soma é de:" ,multi)
 
 #10. Crie duas variáveis. Atribua valores numéricos a elas. Divida os valores dessas variáveis. Por fim exiba em tela o resultado da divisão.
+variavel1= 5
+variavel2=7
+divisao= variavel1/variavel2
+print("A soma é de:" ,divisao)
