414d6683b04e95cfbd940bded550c6f20c995dab
_Malagutt1_ <kauamarcosmalagutti2003@gmail.com>
2024-10-25 03:30:36 -0300

    commit 25/10

---
+++ exercicios/Revisão Prática Avaliação 01.py
@@ -0,0 +1,64 @@
+"""Revisão Prática Avaliação 01 - 23/10/24 Python
+1. Crie um programa que leia dois números inteiros do usuário e exiba a soma entre
+eles."""
+num1=int(input("Insira um numero"))
+num2=int(input("Insira um numero"))
+soma1=num1+num2
+print(soma1)
+"""2. Faça um programa que leia um número e informe se ele é positivo, negativo ou zero."""
+numero3=int(input("Insira um numero"))
+if numero==0:
+    print("Este numero é 0")
+elif numero<0:
+    
+
+"""3. Escreva um programa que leia dois números e exiba o maior deles."""
+
+"""4. Crie um programa que calcule o quadrado de um número fornecido pelo usuário.."""
+
+"""5. Escreva um programa que verifique se um número é par ou ímpar."""
+
+"""6. Crie um programa que peça a idade do usuário e exiba se ele é maior de idade ou
+não."""
+
+"""7. Escreva um programa que calcule a média aritmética de três números fornecidos
+pelo usuário."""
+
+"""8. Faça um programa que leia um valor e informe se ele está entre 10 e 20."""
+
+"""9. Escreva um programa que verifique se um ano é bissexto."""
+
+"""10. Crie um programa que leia um número e informe se ele é divisível por 3 ou por 5."""
+
+"""11. Faça um programa que peça um número inteiro e exiba todos os números de 1 até o
+número fornecido."""
+
+"""12. Crie um programa que leia a temperatura em graus Celsius e converta para
+Fahrenheit. Expressão de conversão: (celsius * 9/5) + 32."""
+
+"""13. Escreva um programa que peça o nome e a idade de uma pessoa e exiba uma
+mensagem de boas-vindas personalizada."""
+
+"""14. Crie um programa que leia dois números e exiba o resultado da multiplicação entre
+eles."""
+
+"""15. Faça um programa que leia três números e exiba o menor deles."""
+
+"""16. Escreva um programa que leia a nota de um aluno e verifique se ele foi aprovado
+(nota >= 6)."""
+
+"""17. Crie um programa que leia o peso e a altura de uma pessoa e calcule seu IMC."""
+
+"""18. Faça um programa que leia um número e exiba sua tabuada (de 1 a 10)."""
+
+"""19. Crie um programa que conte de 1 até 100 e exiba apenas os números pares."""
+
+"""20. Faça um programa que simule uma calculadora simples (soma, subtração,"""
+
+"""multiplicação e divisão) com duas entradas do usuário."""
+
+"""21. Crie um programa que peça um número ao usuário e imprima se ele é primo ou não."""
+
+"""22. Escreva um programa que some todos os números de 1 até 100."""
+
+"""23. Crie um programa que peça um número e calcule seu fatorial."""
