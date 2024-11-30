a68006ca7ee08cbed2fdc0c033505aed1c3a8dc8
_Malagutt1_ <kauamarcosmalagutti2003@gmail.com>
2024-11-30 01:24:11 -0300

    commit data_hora_py

--- biblioteca/data_hora_no_python.py
+++ biblioteca/data_hora_no_python.py
@@ -1,5 +1,5 @@
-from datetime import datetime,date,time 
-
+from datetime import datetime,date,time, timedelta
+import locale
 """ Pytz é uma biblioteca no Python que permite calcular fuso horarios e resolver problemas com 
 horarios amniguos no final do horario de verão"""
 
@@ -20,3 +20,22 @@ print(data)
 print("represente o horario '14:30:59' ")
 horario_especifico=time(14, 30, 59)
 print(horario_especifico)
+
+print("Formatação de data")
+agora=datetime.now()
+data_formatada=agora.strftime("%d/%m/%Y ")
+print(f"A data formatada é: {data_formatada}")
+
+print("Formatação de hora")
+agora=datetime.now()
+hora_formatada=agora.strftime("%H:%M:%S")
+print(f"A hora formatada é: {hora_formatada}")
+
+print("Definir local/região/pais")   #erro line 36!!!!   dando erro line 608?
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+data_hora_no_pais_do_Locale = datetime.now()
+data_formatada_BR=data_hora_no_pais_do_Locale.strftime("%x")
+hora_formatada_BR=data_hora_no_pais_do_Locale.strftime("%X")
+print(f"A data formatada para PT-BR é{data_formatada_BR}")
+print(f"A data formatada para PT-BR é{hora_formatada_BR}")
