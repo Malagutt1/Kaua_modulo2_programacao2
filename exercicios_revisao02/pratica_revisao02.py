d2c3c78814981a6409e2be3e6eff9efdb415dcc2
_Malagutt1_ <kauamarcosmalagutti2003@gmail.com>
2024-12-04 17:27:50 -0300

    commit pratica_revisao02

--- exercicios_revisao02/pratica_revisao02.py
+++ exercicios_revisao02/pratica_revisao02.py
@@ -16,14 +16,14 @@ def lista_de_exercicios_03_questão01():
         if not num.isnumeric():
             print("Valor inválido, você deve inserir um número")
         else:
-            int(num)
+            num= int(num)
             break
     while True: 
         num2=input("Informe o segundo numero:  ")
         if not num2.isnumeric():
             print("Valor inválido, você deve inserir um número")
         else:
-            int(num2)
+            num2=int(num2)
             break
 
     print(f"     ➢os numeros escolhidos foram {num} e {num2}")
@@ -36,26 +36,57 @@ def lista_de_exercicios_03_questão01():
     escolha=input("confirme sua escolha:  ")
     if not escolha.isnumeric():
         print ("Valor invalido, tentar novamente")
+        
     else:
+        escolha=int(escolha)
         if escolha==1:
             soma=num+num2
             #print("o resultado é de ", soma)
-            return "o resultado é de" + str(soma)
+            return "o resultado é de    " + str(soma)
+                   
         elif escolha==2:
             subtracao=num-num2
-
-            print("o resultado é de ", subtracao)
+            return "o resultado é de    "+ str(subtracao)
+            #print("o resultado é de ", +subtracao)
         elif escolha==3:
             multi=num*num2
-            print("o resultado é de ", multi)
+            #print("o resultado é de ", multi)
+            return "o resultado é de    " + str(multi)
         elif escolha==4:
             divisao=num/num2
-            print("o resultado é de ", divisao)
+            #print("o resultado é de ", divisao)
+            return "o resultado é de    " + str(divisao)
+        
 
-    
-    
-    
-    
-    
-lista_de_exercicios_03_questão01()
+retorno_da_funcao=lista_de_exercicios_03_questão01()
+print(retorno_da_funcao)
 
+
+def IMC_funcao():
+    """2) Escreva um programa que calcule o “índice de massa corporal” (IMC).
+IMC = peso em quilos / altura 2. Exiba o resultado na tela. Utilizar valores
+em ponto flutuante, precisão simples."""
+    print("Calculadora de IMC")
+    peso=float(input("Informe seu peso (em Kg) "))
+    altura=float(input("Informe sua altura (em metros!)  "))
+    imc= peso/(altura*altura)
+    return "Seu IMC é de:   " +str(imc)
+retorno_IMC=IMC_funcao()
+print(retorno_IMC)
+
+def procurar_na_frase():
+    """3) Escreva um programa que exiba no terminal a mensagem: “Bem vindo
+    turma da Programação II ao mundo da programação Python!!!” de trás p/
+    a frente. Ou seja, o resultado esperado, deverá ser: !!!nohtyP
+    oãçamargorp ad odnum oa II oãçamargorP ad amrut odniv meB. NÃO
+    UTILIZAR FUNÇÃO P/ RESOLVER ESSA QUESTÃO. A função
+    responsável por imprimir no stdout é uma das formas de resolver a
+    questão."""
+
+    print("\n\nprocurar na frase")
+    frase = ("Bem vindo turma da Programação II ao mundo da programação Python!!!")
+    ######### string=input
+    string = frase[::-1]
+    print('A frase que você digitou invertida fica: {}'.format(string))
+    
+    return
